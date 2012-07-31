0180b0d8d33d6b822495527b9a9befdda596d9ed
Stephen Wright <wright.net@gmail.com>
2012-07-31 16:39:53 -0500

    Initial Commit

---
+++ src/Controllers/TestsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTestPlayer.Controllers
+{
+    public class TestsController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+    }
+}
